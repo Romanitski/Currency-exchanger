2d5d68d769b43a0ad2d67855fabb740484c314d8
Romanitski <vromanitski@gmail.com>
2023-07-10 15:10:43 +0300

    Operator class added.

---
+++ CurrencyExchanger/CurrencyExchanger/Classes/Operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchanger.Classes
+{
+    public class Operator
+    {
+        private static Operator instance;
+
+        private Operator() { }
+
+        public static Operator GetInstance()
+        {
+            return instance ?? (instance = new Operator());
+        }
+
+        public int operatorId { get; set; }
+        public string operatorName { get; set; }
+        public string operatorPassword { get; set; }
+
+        public Dictionary<string, string> operatorType = new Dictionary<string, string>();
+    }
+}
